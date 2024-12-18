622f41fe2f5f29f06b22368ec0ec2d26b156d603
Carolina <Carolina@DESKTOP-EJ6SGIF>
2024-12-18 13:46:27 +0100

    Add project files.

---
+++ dx12_playground/dx12_playground.cpp
@@ -0,0 +1,12 @@
+﻿// dx12_playground.cpp : Defines the entry point for the application.
+//
+
+#include "dx12_playground.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
